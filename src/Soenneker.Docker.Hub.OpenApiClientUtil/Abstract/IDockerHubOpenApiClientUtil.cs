d5efbd95d7a21101d68b3b92a228b24380013bc4
soenneker <jake@soenneker.com>
2026-08-29 14:17:00 -0500

    Improve public API XML documentation

--- src/Soenneker.Docker.Hub.OpenApiClientUtil/Abstract/IDockerHubOpenApiClientUtil.cs
+++ src/Soenneker.Docker.Hub.OpenApiClientUtil/Abstract/IDockerHubOpenApiClientUtil.cs
@@ -2,13 +2,16 @@ using Soenneker.Docker.Hub.OpenApiClient;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace Soenneker.Docker.Hub.OpenApiClientUtil.Abstract;
-
 /// <summary>
 /// Exposes a cached OpenAPI client instance.
 /// </summary>
 public interface IDockerHubOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured docker Hub OpenAPI Client used by the Docker Hub OpenAPI Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested docker Hub OpenAPI Client.</returns>
     ValueTask<DockerHubOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Docker.Hub.OpenApiClientUtil/Registrars/DockerHubOpenApiClientUtilRegistrar.cs
+++ src/Soenneker.Docker.Hub.OpenApiClientUtil/Registrars/DockerHubOpenApiClientUtilRegistrar.cs
@@ -13,6 +13,8 @@ public static class DockerHubOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="DockerHubOpenApiClientUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddDockerHubOpenApiClientUtilAsSingleton(this IServiceCollection services)
     {
         services.AddDockerHubOpenApiHttpClientAsSingleton()
@@ -24,6 +26,8 @@ public static class DockerHubOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="DockerHubOpenApiClientUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddDockerHubOpenApiClientUtilAsScoped(this IServiceCollection services)
     {
         services.AddDockerHubOpenApiHttpClientAsSingleton()
