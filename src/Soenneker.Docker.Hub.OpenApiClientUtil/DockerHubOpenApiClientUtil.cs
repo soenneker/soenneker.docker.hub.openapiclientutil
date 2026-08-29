f9e08fface7aaef803cdfc9ce48fd5381b3a61e1
soenneker <jake@soenneker.com>
2026-08-29 14:50:33 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Docker.Hub.OpenApiClientUtil/DockerHubOpenApiClientUtil.cs
+++ src/Soenneker.Docker.Hub.OpenApiClientUtil/DockerHubOpenApiClientUtil.cs
@@ -14,7 +14,7 @@ using Soenneker.Utils.AsyncSingleton;
 
 namespace Soenneker.Docker.Hub.OpenApiClientUtil;
 
-///<inheritdoc cref="IDockerHubOpenApiClientUtil"/>
+/// <inheritdoc cref="IDockerHubOpenApiClientUtil"/>
 public sealed class DockerHubOpenApiClientUtil : IDockerHubOpenApiClientUtil
 {
     private readonly AsyncSingleton<DockerHubOpenApiClient> _client;
